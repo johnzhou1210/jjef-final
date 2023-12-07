c0ff4c9badea06d2e3c9046aaf987082dcbf2462
John Zhou <83607969+johnzhou1210@users.noreply.github.com>
2023-12-07 18:50:17 -0500

    Created basic sidebar along with title and textbox for adding new entry
    Changes made:
    - Created basic sidebar with non-functioning home and list selection page entries.
    - Created barebones title and a button and text field for adding a new entry to the todo list.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import './App.css';
+import "./App.css";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
     <div className="App">
+      <Sidebar />
+
+      <div className="main-content">
+        <h1 className="page-title">To-do</h1>
+        <div className="entry-add-input">
+          <label>Add task</label>
+          <input type="text" id="new-entry-name" name="new-entry-field"></input>
+        </div>
+        <div className="list-content"></div>
+      </div>
     </div>
   );
 }
---
+++ src/Sidebar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Sidebar() {
+  return (
+    <div className='sidebar'>
+        <h2>Sidebar Content</h2>
+        
+        <div className='sidebar-button'>Home</div>
+        <div className='sidebar-button'>Select List</div>
+      
+        
+    </div>
+  )
+}
+
+export default Sidebar
