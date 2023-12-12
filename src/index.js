057e5115fdebb8214240941bd680d2a99f059b34
John Zhou <chowjohn10@gmail.com>
2023-12-12 16:49:29 -0500

    Updated root
    Changes made:
    - Modified root from /JJEFTodoList to just /

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import {createBrowserRouter, RouterProvider} from "react-router-dom"
 const router = createBrowserRouter (
   [
     {
-      path: "/JJEFTodoList",
+      path: "/",
       element: <App />,
       errorElement: <ErrorPage />,
       children: [],
