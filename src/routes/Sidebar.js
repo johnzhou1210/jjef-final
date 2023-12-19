c4d53f9bd8f5c14f85099d4fde6ca72b3843a7cd
farhanaa-noor <80719003+farhanaa-noor@users.noreply.github.com>
2023-12-18 21:06:48 -0500

    Update Sidebar.js
    a bar at the top to name the newly created list, a MyList section at the bottom with all the active list

--- src/routes/Sidebar.js
+++ src/routes/Sidebar.js
@@ -1,59 +1,41 @@
 import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { setActiveList, addList, removeList } from '../app/userListsSlice';
-import { Link } from 'react-router-dom';
 
 function Sidebar() {
-  const dispatch = useDispatch();
-  const { lists, activeListId } = useSelector((state) => state.userLists);
-
+  const [lists, setLists] = useState([]);
   const [newListName, setNewListName] = useState('');
 
-  const handleListClick = (listId) => {
-    dispatch(setActiveList(listId));
-  };
-
-  const handleNewListClick = () => {
+  const handleCreateList = () => {
     if (newListName.trim() !== '') {
-      dispatch(addList({ name: newListName }));
+      setLists([...lists, newListName]);
       setNewListName('');
     }
-  };
-
-  const handleRemoveList = (listId) => {
-    dispatch(removeList(listId));
   };
 
   return (
     <div className='sidebar'>
       <h2>Navigation</h2>
-      {/* New List Button */}
-      <div>
-        <input
-          type="text"
-          placeholder="Enter list name"
-          value={newListName}
-          onChange={(e) => setNewListName(e.target.value)}
-        />
-        <button className='new-list-button' onClick={handleNewListClick}>
-          New List
-        </button>
-      </div>
-      <div className='sidebar-button'>Home</div>
 
-      <h3>My Lists</h3>
-      <ul className="lists">
-        {lists.map((list) => (
-          <li
-            key={list.id}
-            onClick={() => handleListClick(list.id)}
-            className={list.id === activeListId ? 'active-list' : ''}
-          >
-            {list.name || `List ${list.id}`}
-            {/*<button onClick={() => handleRemoveList(list.id)}>Delete</button>*/}
-          </li>
-        ))}
-      </ul>
+      <div className='sidebar-button'>
+        <div>
+          <label>Create New List:</label>
+          <input
+            type='text'
+            placeholder='New List Name'
+            value={newListName}
+            onChange={(e) => setNewListName(e.target.value)}
+          />
+          <button onClick={handleCreateList}>Create List</button>
+        </div>
+      </div>
+
+      <div className='sidebar-button'>
+        <h3>MyList</h3>
+        <ul>
+          {lists.map((list, index) => (
+            <li key={index}>{list}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
