91dcb0967c5ed846157bfe0d0052793edb33d705
John Zhou <chowjohn10@gmail.com>
2023-12-19 03:28:19 -0500

    Integrated Entry component into List
    Changes made:
    - Replaced temporary div with Entry component.
    - Added styling to make entries look better.

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -1,41 +1,57 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-function Entry({ completed, text, priority, color, list_id, date_created, entry_id }) {
-    const [entryCompleted, setEntryCompleted] = useState(completed);
-    const [entryText, setEntryText] = useState(text);
-    const [currentEntryId, setCurrentEntryId] = useState(entry_id);
+function Entry({
+  completed,
+  text,
+  priority,
+  color,
+  list_id,
+  date_created,
+  entry_id,
+}) {
+  const [entryCompleted, setEntryCompleted] = useState(completed);
+  const [entryText, setEntryText] = useState(text);
+  const [currentEntryId, setCurrentEntryId] = useState(entry_id);
 
-    const update_endpoint = "http://localhost:3001/updateEntry/";
+  const update_endpoint = "http://localhost:3001/updateEntry/";
 
-    function updateEntryComplete() {
-        fetch(update_endpoint + currentEntryId, {
-            method: "PUT",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              completed: entryCompleted,
-            }),
-          })
-            .then((response) => console.log("Edited entry: " + response.status))
-            .catch((error) => console.error(error));
-    }
+  function updateEntryComplete() {
+    fetch(update_endpoint + currentEntryId, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        completed: !entryCompleted,
+      }),
+    })
+      .then((response) => console.log("Edited entry: " + response.status))
+      .catch((error) => console.error(error));
+  }
 
-    function checkCompletedEntry() {
-        setEntryCompleted(!entryCompleted);
-        updateEntryComplete();
-    }
+  function checkCompletedEntry() {
+    setEntryCompleted(!entryCompleted);
+    updateEntryComplete();
+  }
 
-    return (
+  return (
+    <div className="list-entry">
+      {currentEntryId === null ||
+      entryCompleted === null ||
+      entryText === null ? (
+        "Loading..."
+      ) : (
         <>
-            {currentEntryId === null || entryCompleted === null || entryText === null ? "Loading..." : 
-                <>
-                    <button onClick={() => checkCompletedEntry()}>{!entryCompleted ? "o" : "x"}</button>
-                    <p>{entryText}</p>
-                </>
-            }
+          <button onClick={() => checkCompletedEntry()}>
+            <div className="checkbox">{!entryCompleted ? "☐" : "☑"}</div>
+          </button>
+          <div className={entryCompleted ? "strikethrough entry-text" : "entry-text"}>
+            {entryText}
+          </div>
         </>
-    );
+      )}
+    </div>
+  );
 }
 
 export default Entry;
--- src/routes/App.js
+++ src/routes/App.js
@@ -1,16 +1,45 @@
 import "../App.css";
 import Sidebar from "./Sidebar";
 import List from "./List";
+import { useDispatch, useSelector } from "react-redux";
+import { setData } from "../app/listSlice";
 
 function App() {
+  const currList = useSelector((state) => state.list.data); // returns current list object json
+  const currListId = 1; // this is only temporary, replace this once set current get and update
+  const dispatch = useDispatch();
+
+  async function updateListSlice() {
+    // updates the data in redux by fetching from the database.
+    var requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch("http://localhost:3001/getList/" + currListId, requestOptions)
+      .then((response) => response.json())
+      .then((resJson) => dispatch(setData(resJson)))
+      .catch((error) => console.log("error", error));
+  }
+
+
   return (
     <div className="App">
       <Sidebar />
 
       <div className="main-content">
         <h1 className="page-title">To-do</h1>
-        <List />
+
+        {false ? <NoActiveListsNotice /> : <List updateListSlice={updateListSlice} />}
       </div>
+    </div>
+  );
+}
+
+function NoActiveListsNotice() {
+  return (
+    <div className="large-notice">
+      There are no active lists. Choose a list on the sidebar or create a new one!
     </div>
   );
 }
