800ca4a5ed11fb292e51472e78b7e5734019d4fa
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2023-12-18 22:58:22 -0500

    created some parts of Entry component

---
+++ src/components/Entry.js
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+
+function Entry({ completed, text, priority, color, list_id, entry_id }) { // date_created
+    const [entryCompleted, setEntryCompleted] = useState(completed);
+    const [entryText, setEntryText] = useState(text);
+    const [currentEntryId, setCurrentEntryId] = useState(entry_id);
+
+    const update_endpoint = "http://localhost:3001/updateEntry/";
+
+    function updateEntryComplete() {
+        fetch(update_endpoint + currentEntryId, {
+            method: "PUT",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              completed: entryCompleted
+            }),
+          })
+            .then((response) => console.log("Edited entry: " + response.status))
+            .catch((error) => console.error(error));
+    }
+
+    function checkCompletedEntry() {
+        setEntryCompleted(!entryCompleted);
+        updateEntryComplete();
+    }
+
+    return (
+        <>
+            {currentEntryId === null || entryCompleted === null || entryText === null ? "Loading..." : 
+                <>
+                    <button onClick={() => checkCompletedEntry()}>{!entryCompleted ? "o" : "x"}</button>
+                    <p>{text}</p>
+                </>
+            }
+        </>
+    );
+}
+
+export default Entry;
