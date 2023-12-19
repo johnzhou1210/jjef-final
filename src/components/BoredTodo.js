cc70295eec6afe16c495a230c99531cb59bfdcd6
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2023-12-18 21:16:30 -0500

    Create BoredTodo component

---
+++ src/components/BoredTodo.js
@@ -0,0 +1,22 @@
+function BoredTodo({ handleCreateBoredEntry }) {
+
+    // a random activity will be returned
+    const bored_endpoint = "https://www.boredapi.com/api/activity/";
+
+    function handleGetBoredActivity() {
+        fetch(bored_endpoint)
+            .then(response => response.json())
+                .then(jsonData =>  handleCreateBoredEntry(jsonData.activity))
+                    .catch(error => console.error(error));
+    }
+
+    return (
+        <>
+            <button onClick={() => handleGetBoredActivity()}>
+                Add a random activity to your list
+            </button>
+        </>
+    );
+}
+
+export default BoredTodo;
