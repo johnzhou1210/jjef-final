dbc367f4c3b3f13edd90ca8f49bbfa017f6980c8
John Zhou <chowjohn10@gmail.com>
2023-12-19 15:32:00 -0500

    Make delete-entry changes show up immediately
    Changes made:
    - Delete entry changes now show up after deleting the entry without needing to refresh the page.
    - "Add to which list?" now automatically updates based on the current active list.
    - Added some CSS to make the edit and delete buttons, as well as the edit text field look better.
    - Added `newEntryTargetListId` to `listSlilce.js` as a state to automatically change list target for entry creation to current active list.

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setData } from "../app/listSlice";
 
 function Entry({
   completed,
@@ -16,6 +18,7 @@ function Entry({
   const [edit, setEdit] = useState(false);
   const [prevEntry, setPrevEntry] = useState("");
 
+  const dispatch = useDispatch();
   const update_endpoint = "http://localhost:3001/updateEntry/";
 
   const delete_endpoint = "http://localhost:3001/deleteEntry/";
@@ -69,8 +72,10 @@ function Entry({
     fetch(delete_endpoint + currentEntryId, {
       method: "DELETE",
     })
-      .then((response) => console.log("Deleted entry: " + response.status))
+      .then((response) => console.log("Deleted entry: " + entryText))
       .catch((error) => console.error(error));
+    dispatch(setData(null)); // needed or else there is this really weird bug
+    updateListSlice();
   }
 
   useEffect(() => {
@@ -95,10 +100,26 @@ function Entry({
               entryCompleted ? "strikethrough entry-text" : "entry-text"
             }
           >
-            {edit ? <input value={entryText} onChange={modifyEntryText}></input>: <p>{entryText}</p>}
+            {edit ? (
+              <p>
+                <input
+                  className="entry-text"
+                  value={entryText}
+                  onChange={modifyEntryText}
+                />
+              </p>
+            ) : (
+              <p>{entryText}</p>
+            )}
           </div>
-          <button onClick={() => editEntry()}>{edit ? "complete edit" : "edit"}</button>
-          <button onClick={() => deleteEntry()}>delete</button>
+          <div className="entry-buttons">
+            <button className="list-entry-button" onClick={() => editEntry()}>
+              {edit ? "OK" : "Edit"}
+            </button>
+            <button className="list-entry-button" onClick={() => deleteEntry()}>
+              Delete
+            </button>
+          </div>
         </>
       )}
     </div>
